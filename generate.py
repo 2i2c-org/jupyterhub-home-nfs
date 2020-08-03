1501c78fbd53ea5e5c1c56ff37c929a1e32e1572
YuviPanda <yuvipanda@gmail.com>
2020-08-03 14:57:22 +0530

    Rewrite to be a proper reconciliation loop
    Far easier to reason about!

--- generate.py
+++ generate.py
@@ -24,11 +24,21 @@ This script will:
 
 Your home directories must already be on an XFS file system, with prjquota
 mount option enabled.
+
+Runs as a reconciliation loop. It tries to keep the following pieces in
+sync:
+
+1. List of home directories (from paths given)
+2. Project ID files `/etc/project` and `/etc/projids`
+3. XFS quota project setup presence (via `xfs_quota`)
+4. XFS quota limits given.
 """
+import sys
 import os
 import time
 import subprocess
 import argparse
+import itertools
 
 
 def parse_projids(path):
@@ -43,26 +53,6 @@ def parse_projids(path):
                 projects[splits[0]] = int(splits[1])
     return projects
 
-
-def populate_projects(path, projects, min_projid):
-    changed = []
-    for ent in os.scandir(path):
-        if ent.path not in projects:
-            if projects:
-                new_id = max(projects.values()) + 1
-            else:
-                new_id = min_projid
-            projects[ent.path] = new_id
-            print(f"Wrote new project {ent.path} with id {new_id}")
-            changed.append(ent.path)
-    return changed
-
-def xfs_quota_admin(command, mountpoint):
-    """
-    Run arbitrary xfs_quota command on a given mountpoint
-    """
-    subprocess.check_call(['xfs_quota', '-x', '-c', command, mountpoint])
-
 def mountpoint_for(path):
     """
     Return mount point containing file / directory in path
@@ -70,6 +60,32 @@ def mountpoint_for(path):
     xfs_quota wants to know which fs to operate on
     """
     return subprocess.check_output(['df', '--output=target', path]).decode().strip().split('\n')[-1].strip()
+
+
+def get_quotas():
+    output = subprocess.check_output([
+        'xfs_quota', '-x', '-c',
+        'report -N -p'
+    ]).decode().strip()
+    quotas = {}
+    for line in output.split('\n'):
+        path, used, soft, hard, warn, grace = line.split()
+        # Everything here is in kb, since that's what xfs_quota reports things in
+        quotas[path] = {
+            'used': int(used),
+            'soft': int(soft),
+            'hard': int(hard),
+            'warn': int(warn),
+            'grace': grace
+        }
+    return quotas
+
+
+def write_projfiles(projects, projects_file_path, projid_file_path):
+    with open(projects_file_path, 'w') as projects_file, open(projid_file_path, 'w') as projid_file:
+        for path, id in projects.items():
+            projid_file.write(f'{path}:{id}\n')
+            projects_file.write(f'{id}:{path}\n')
 
 def main():
 
@@ -85,24 +101,61 @@ def main():
     argparser.add_argument('--wait-time', default=30, type=int,
         help='Number of seconds to wait between runs'
     )
+    argparser.add_argument('--hard-quota', default=10, type=float,
+        help='Hard quota limit (in GB) to set for all home directories'
+    )
     args = argparser.parse_args()
 
-    while True:
-        projects = parse_projids(args.projid_file)
-        changed = []
-        for path in args.paths:
-            changed += populate_projects(path, projects, args.min_projid)
+    # xfs_quota reports in kb
+    hard_quota_kb = int(args.hard_quota * 1024 * 1024)
 
-        if changed:
-            print(f"New projects, setting up quotas")
-            with open(args.projects_file, 'w') as projects_file, open(args.projid_file, 'w') as projid_file:
-                for path, id in projects.items():
-                    projid_file.write(f'{path}:{id}\n')
-                    projects_file.write(f'{id}:{path}\n')
-            for changed_project in changed:
-                mountpoint = mountpoint_for(changed_project)
-                xfs_quota_admin(f'project -s {changed_project}', mountpoint)
-                xfs_quota_admin(f'limit -p bhard=20g {changed_project}', mountpoint)
+    while True:
+        # Fetch existing home directories
+        homedirs = [ent.path for path in args.paths for ent in os.scandir(path) if ent.is_dir()]
+        # Fetch list of projects in /etc/projid file, assumed to sync'd to /etc/projects file
+        projects = parse_projids(args.projid_file)
+        # Fetch quota information from xfs_quota
+        quotas = get_quotas()
+
+        # Check if there are any homedirs that aren't in projects
+        changed_entries = [h for h in homedirs if h not in projects]
+        # Check for home directories that don't have correct quota
+        changed_entries += [h for h in homedirs if h in quotas and quotas[h]['hard'] != hard_quota_kb]
+
+        # Make sure /etc/projid & /etc/projects are in sync with home dirs
+        if changed_entries:
+            projid_file_dirty = False
+            for entry in changed_entries:
+                # Ensure an entry exists in projects
+                if entry not in projects:
+                    projects[entry] = max(projects.values() or [args.min_projid]) + 1
+                    projid_file_dirty = True
+                    print(f'Found new project {entry}')
+
+            if projid_file_dirty:
+                write_projfiles(projects, args.projects_file, args.projid_file)
+                print(f'Writing /etc/projid and /etc/projects')
+
+        # Make sure xfs_quotas is in sync
+        # /etc/projid & /etc/projects need to be already in sync before this can work
+        if changed_entries:
+            for entry in changed_entries:
+                mountpoint = mountpoint_for(entry)
+                if entry not in quotas:
+                    subprocess.check_call([
+                        'xfs_quota', '-x', '-c',
+                        f'project -s {entry}',
+                        mountpoint
+                    ])
+                    print(f'Setting up xfs_quota project for {entry}')
+                if entry not in quotas or quotas[entry]['hard'] != hard_quota_kb:
+                    print(quotas[entry])
+                    subprocess.check_call([
+                        'xfs_quota', '-x', '-c',
+                        f'limit -p bhard={hard_quota_kb}k {entry}',
+                        mountpoint
+                    ])
+                    print(f'Setting limit for project {entry} to {hard_quota_kb}k')
 
         time.sleep(args.wait_time)
 
